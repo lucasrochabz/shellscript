338278ab86a3e0b4251d75e537acc281aaa83512
lucasrochabz <lucasbezerrar@gmail.com>
2025-01-14 15:38:10 -0300

    add atribuicao de conteudo para arquivos

--- automacao-de-tarefas/script.sh
+++ automacao-de-tarefas/script.sh
@@ -14,5 +14,29 @@ touch index.html
 touch style.css
 touch script.js
 
+# Atribuição de conteúdo para arquivos
+echo "<!DOCTYPE html>
+<html lang=\"pt-br\">
+
+<head>
+  <meta charset=\"UTF-8\">
+  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+  <link rel=\"stylesheet\" href=\"style.css\">
+  <title>Automação de tarefas</title>
+</head>
+
+<body>
+  <h1>Hello World!</h1>
+  <script src=\"script.js\"></script>
+</body>
+
+</html>" > index.html
+
+echo "h1 {
+  background-color: blue;
+}" > style.css
+
+echo "console.log('O projeto foi carergado com sucesso!');" > script.js
+
 # Saída de informações
 echo "Projeto criado em $path/$project_name"
